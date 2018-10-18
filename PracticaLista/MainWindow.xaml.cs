946f16a43e3253b58024ecbc287868108c387b8b
david arturo preciado osuna <arturo_preciado@live.com.mx>
2018-10-18 08:31:57 -0700

    se agregó otro listbox para las materias

--- PracticaLista/MainWindow.xaml.cs
+++ PracticaLista/MainWindow.xaml.cs
@@ -26,12 +26,32 @@ namespace PracticaLista
         public MainWindow()
         {
             InitializeComponent();
+
+            Materia historia = new Materia("HST123", "Historia");
+            Materia matematicas = new Materia("MAT456", "Matematicas");
+            Materia civismo = new Materia("CIV741", "Civismo");
+            Materia español = new Materia("ESP963", "Español");
+            Materia artistica = new Materia("ART852", "Artistica");
+
             alumnos.Add(new Alumno("Jose Pérez", "153697", "Lic. en Derecho"));
             alumnos.Add(new Alumno("Juan López", "581269", "Lic. en Psicologia"));
             alumnos.Add(new Alumno("Cristian García", "153697", "Lic. en Finanzas"));
             alumnos.Add(new Alumno("María Valenzuela", "142536", "Ing. Civil"));
 
-            foreach(Alumno alumno in alumnos)
+            //Primer alumno
+            alumnos[0].Materias.Add(español);
+            alumnos[0].Materias.Add(artistica);
+            //Segundo
+            alumnos[1].Materias.Add(civismo);
+            alumnos[1].Materias.Add(matematicas);
+            //Tercero
+            alumnos[2].Materias.Add(historia);
+            alumnos[2].Materias.Add(español);
+            //Cuarto
+            alumnos[3].Materias.Add(civismo);
+            alumnos[3].Materias.Add(artistica);
+
+            foreach (Alumno alumno in alumnos)
             {
                 lstAlumnos.Items.Add(new ListBoxItem() {Content = alumno.Nombre} );
             }
@@ -42,6 +62,22 @@ namespace PracticaLista
             lblNombre.Text = alumnos[lstAlumnos.SelectedIndex].Nombre;
             lblMatricula.Text = alumnos[lstAlumnos.SelectedIndex].Matricula;
             lblCarrera.Text = alumnos[lstAlumnos.SelectedIndex].Carrera;
+
+            lstMaterias.Items.Clear();
+
+            foreach (Materia materia in alumnos[lstAlumnos.SelectedIndex].Materias)
+            {
+                lstMaterias.Items.Add(new ListBoxItem() { Content = materia.Nombre });
+            }
+        }
+
+        private void lstMaterias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(lstMaterias.SelectedIndex != -1)
+            {
+                lblClave.Text = alumnos[lstAlumnos.SelectedIndex].Materias[lstMaterias.SelectedIndex].Clave;
+                lblNombreMateria.Text = alumnos[lstAlumnos.SelectedIndex].Materias[lstMaterias.SelectedIndex].Nombre;
+            }
         }
     }
 }
---
+++ PracticaLista/Materia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaLista
+{
+    class Materia
+    {
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+
+        public int CalificacionPrimerParcial { get; set; }
+        public int CalificacionSegundoParcial { get; set; }
+        public int CalificacionTercerParcial { get; set; }
+        public int CalificacionFinal { get; set; }
+
+        public Materia(string clave, string nombre)
+        {
+            Clave = clave;
+            Nombre = nombre;
+
+            CalificacionPrimerParcial = 0;
+            CalificacionSegundoParcial = 0;
+            CalificacionTercerParcial = 0;
+            CalificacionFinal = 0;
+        }
+
+        public Materia(string clave, string nombre, int calificacionPrimerParcial, int calificacionSegundoParical, int calificacionTerecerParcial, int calificacionFinal)
+        {
+            Clave = clave;
+            Nombre = nombre;
+            CalificacionPrimerParcial = calificacionPrimerParcial;
+            CalificacionSegundoParcial = calificacionSegundoParical;
+            CalificacionTercerParcial = calificacionTerecerParcial;
+            CalificacionFinal = calificacionFinal;
+        }
+    }
+}
